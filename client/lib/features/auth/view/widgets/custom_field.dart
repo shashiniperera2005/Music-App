b38d2bf42f7e23076654763c38d421e494ef95b2
Shashini Perera <shashini.20230303@iit.ac.lk>
2025-04-02 03:11:15 +0530

    sign in & out pages done.

---
+++ client/lib/features/auth/view/widgets/custom_field.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CustomField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+  final bool isObscureText;
+  const CustomField({
+    super.key,
+    required this.hintText,
+    required this.controller,
+    this.isObscureText = false,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+      ),
+      validator: (val){
+        if (val!.trim().isEmpty){
+          return "$hintText is missing!";
+        }
+        return null;
+      },
+      obscureText: isObscureText,
+    );
+  }
+}
