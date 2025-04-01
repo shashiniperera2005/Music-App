56ffd2082bd0adb81820a400e761badbd368ce83
Shashini Perera <shashini.20230303@iit.ac.lk>
2025-04-02 03:34:46 +0530

    updated title

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Music App',
       theme: AppTheme.dartkThemeMode,
       home: const LoginPage(),
     );
